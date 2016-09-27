705a8c1bd1ecde0629a15d90f6e327d1114bd882
Guanlong Zhou <zhouguanlong2012@gmail.com>
2016-09-27 17:56:47 -0500

    Basic Function completes
    Added labels and text fields and controls.
    Basic function built, can calculate 18% 20% 22% tips.
    ToDo: Boundary case , negative number and too big number.

--- Tip-Calculator/Tip-Calculator/ViewController.swift
+++ Tip-Calculator/Tip-Calculator/ViewController.swift
@@ -3,6 +3,7 @@
 //  Tip-Calculator
 //
 //  Created by Guanlong Zhou on 9/26/16.
+//  Github: mookerz
 //  Copyright © 2016 Guanlong Zhou. All rights reserved.
 //
 
@@ -10,6 +11,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    let tipPercentages = [0.18, 0.20, 0.22]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +29,24 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onTapMainScreen(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: AnyObject) {
+        let bill : Double = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = tip + bill
+        
+        
+        tipLabel.text = String(format:"$%.2f", tip)
+        totalLabel.text = String(format:"$%.2f", total)
+    }
+    
+    
+    
+    
+    
+    
 }
 
