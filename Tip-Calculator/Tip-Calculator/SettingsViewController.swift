b6e0a0795b087f5d18d3a27173c17b30ef8fbfb3
Guanlong Zhou <zhouguanlong2012@gmail.com>
2016-09-28 21:15:08 -0500

    Optional Function Completed
    Added local currency symbol based on system configuration.
    Added slide bar percentage in the settings.
    Add different slide bar controller in the main menu if configured in
    the setting view.
    Added new gif images.
    Updated README.md

--- Tip-Calculator/Tip-Calculator/SettingsViewController.swift
+++ Tip-Calculator/Tip-Calculator/SettingsViewController.swift
@@ -10,12 +10,56 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    var defaultTipRateValue : Int = 20
+    var isUseSimpleSelection = true
+    
     @IBOutlet weak var defaultTipRateController: UISegmentedControl!
     
+    @IBOutlet weak var defaultTipRateLabel: UILabel!
+    
+    @IBOutlet weak var defaultTipRateSlideBar: UISlider!
+    
+    @IBOutlet weak var simpleRateSwitch: UISwitch!
+    
+    @IBAction func simpleRateSwitchChanged(_ sender: AnyObject) {
+        isUseSimpleSelection = simpleRateSwitch.isOn
+        if(isUseSimpleSelection){
+            if(defaultTipRateValue>=22){
+                defaultTipRateController.selectedSegmentIndex = 2
+                defaultTipRateValue = 22
+            }
+            else if(defaultTipRateValue<=18){
+                defaultTipRateController.selectedSegmentIndex = 0
+                defaultTipRateValue = 18
+            }
+            else{
+                defaultTipRateController.selectedSegmentIndex = 1
+                defaultTipRateValue = 20
+            }
+            
+        }
+        else{
+            defaultTipRateSlideBar.value = Float(defaultTipRateValue)
+        }
+        refreshSettingsView()
+    }
+    
+    func refreshSettingsView(){
+        defaultTipRateController.isHidden = !isUseSimpleSelection
+        defaultTipRateSlideBar.isHidden = isUseSimpleSelection
+        defaultTipRateLabel.text = String(defaultTipRateValue) + "%"
+        simpleRateSwitch.isOn = isUseSimpleSelection
+        defaultTipRateSlideBar.value = Float(defaultTipRateValue)
+        if(isUseSimpleSelection){
+            let tipRates : [Int] = [18, 20, 22]
+            defaultTipRateController.selectedSegmentIndex = tipRates.index(of: defaultTipRateValue) ?? 0
+        }
+        
+        saveStatus()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
        //defaultTipRateController.selectedSegmentIndex
         // Do any additional setup after loading the view.
     }
@@ -27,21 +71,39 @@ class SettingsViewController: UIViewController {
     
     
     @IBAction func setDefaultTipRate(_ sender: AnyObject) {
-        
-        let defaults = UserDefaults.standard
-        defaults.set(defaultTipRateController.selectedSegmentIndex, forKey:"defaultSelectedTipIndex")
-        defaults.synchronize()
+        if(isUseSimpleSelection){
+            let simpleRates = [18, 20, 22]
+            defaultTipRateValue = simpleRates[defaultTipRateController.selectedSegmentIndex]
+        }
+        else{
+            defaultTipRateValue = Int(defaultTipRateSlideBar.value)
+        }
+        refreshSettingsView()
+        saveStatus()
+    }
+    
+    func saveStatus(){
+    let defaults = UserDefaults.standard
+    defaults.set(defaultTipRateValue, forKey:"defaultSelectedTipRate")
+    defaults.set(isUseSimpleSelection,forKey:"isUseSimpleSelection")
+    defaults.synchronize()
+    
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         let defaults = UserDefaults.standard
+        if((defaults.object(forKey: "isUseSimpleSelection")) != nil){
+            defaultTipRateValue = defaults.integer(forKey: "defaultSelectedTipRate")
+            
+            isUseSimpleSelection = defaults.bool(forKey: "isUseSimpleSelection")
+        }
+        else{
+           saveStatus()
+        }
         
-        let defaultIndex = defaults.integer(forKey: "defaultSelectedTipIndex")
-        
-        defaultTipRateController.selectedSegmentIndex = defaultIndex
-        
+        refreshSettingsView()
     }
     
     /*
--- Tip-Calculator/Tip-Calculator/ViewController.swift
+++ Tip-Calculator/Tip-Calculator/ViewController.swift
@@ -16,8 +16,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var settingsButton: UIBarButtonItem!
+    @IBOutlet weak var tipRateSlideBar: UISlider!
+    @IBOutlet weak var tipRateLabel: UILabel!
+    @IBOutlet weak var keyboardStatusLabel: UILabel!
     
-    let tipPercentages = [0.18, 0.20, 0.22]
+    let tipPercentages = [18, 20, 22]
+    var tipRateValue : Int = 20
+    var isUseSimpleSelection = true
+    var isKeyboardOn = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,30 +35,69 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func setKeyboardTextOn(isKeyboardOn: Bool){
+        if(isKeyboardOn){
+            keyboardStatusLabel.text = "Keyboard: On"
+            keyboardStatusLabel.textColor = UIColor.black
+            keyboardStatusLabel.backgroundColor = UIColor.lightGray
+        }else{
+            keyboardStatusLabel.text = "Keyboard: Off"
+            keyboardStatusLabel.textColor = UIColor.black
+            keyboardStatusLabel.backgroundColor = UIColor.white
+        }
+    }
+    
+    @IBAction func billFieldEditingBegin(_ sender: AnyObject) {
+        setKeyboardTextOn(isKeyboardOn: true)
+        isKeyboardOn = true
+    }
 
     @IBAction func onTapMainScreen(_ sender: AnyObject) {
-        view.endEditing(true)
+        if(isKeyboardOn){
+            view.endEditing(true)
+            isKeyboardOn = false
+        }
+        else{
+            billField.becomeFirstResponder()
+            isKeyboardOn = true
+        }
+        setKeyboardTextOn(isKeyboardOn: isKeyboardOn)
     }
     
     @IBAction func calculateTip(_ sender: AnyObject) {
-        let bill : Double = Double(billField.text!) ?? 0
+        var bill: Double = Double(billField.text!) ?? 0
+        
         if(bill > 9999999){
-            let alertController = UIAlertController(title: "iOScreator", message:
+            let alertController = UIAlertController(title: "Warning!", message:
                 "Number is too big!", preferredStyle: UIAlertControllerStyle.alert)
-            alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default,handler: nil))
             
             self.present(alertController, animated: true, completion: nil)
+            billField.text = ""
+            bill = 0
         }
         
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        if(isUseSimpleSelection){
+            tipRateValue = tipPercentages[tipControl.selectedSegmentIndex]
+        }
+        else{
+            tipRateValue = Int(tipRateSlideBar.value)
+        }
+        
+        
+        let tip : Double = bill * Double(tipRateValue)/100
         let total = tip + bill
         
+        let formatter = NumberFormatter()
+        formatter.numberStyle = NumberFormatter.Style.currency
         
-        tipLabel.text = String(format:"$%.2f", tip)
-        totalLabel.text = String(format:"$%.2f", total)
+        
+        tipLabel.text = formatter.string(from: NSNumber.init(value: tip))
+        totalLabel.text = formatter.string(from: NSNumber.init(value: total))
         
         let defaults = UserDefaults.standard
         defaults.set(billField.text!, forKey:"presetBillValue")
+        refreshCalculatorView()
         defaults.synchronize()
     }
     
@@ -61,16 +106,42 @@ class ViewController: UIViewController {
         super.viewWillAppear(animated)
         
         let defaults = UserDefaults.standard
+        if((defaults.object(forKey: "isUseSimpleSelection")) != nil){
+            tipRateValue = defaults.integer(forKey: "defaultSelectedTipRate")
+            
+            isUseSimpleSelection = defaults.bool(forKey: "isUseSimpleSelection")
+            
+            billField.text = defaults.string(forKey: "presetBillValue")
+        }
+        else{
+            saveStatus()
+        }
         
-        let defaultIndex = defaults.integer(forKey: "defaultSelectedTipIndex")
-        
-        billField.text = defaults.string(forKey: "presetBillValue") ?? "0"
-        
-        tipControl.selectedSegmentIndex = defaultIndex
+        refreshCalculatorView()
+
         calculateTip("Calculate!" as AnyObject)
         
     }
     
+    func refreshCalculatorView(){
+        tipControl.isHidden = !isUseSimpleSelection
+        tipRateSlideBar.isHidden = isUseSimpleSelection
+        tipRateLabel.text = String(tipRateValue) + "%"
+        tipRateSlideBar.value = Float(tipRateValue)
+        if(isUseSimpleSelection){
+            tipControl.selectedSegmentIndex = tipPercentages.index(of: Int(tipRateValue)) ?? 0
+        }
+        tipRateLabel.text = String(tipRateValue) + "%"
+        saveStatus()
+    }
+    
+    func saveStatus(){
+        let defaults = UserDefaults.standard
+        //defaults.set(tipRateValue, forKey:"defaultSelectedTipRate")
+        defaults.set(isUseSimpleSelection,forKey:"isUseSimpleSelection")
+        defaults.synchronize()
+        
+    }
     
     func applicationDidEnterBackground(application: UIApplication) {
 
